29444bd5cdbcd3903bfd6693aadad225eb3852d1
Archi Halder <archihalder23026558@gmail.com>
2025-07-19 22:31:22 +0530

    minor changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
-
 const Search = ({searchTerm, setSearchTerm}) => {
     return (
         <div className="search">
             <div>
                 <img src="../../public/search.svg" alt="Search Icon"/>
-
                 <input
                     type="text"
                     placeholder="Search through thousands of movies"
